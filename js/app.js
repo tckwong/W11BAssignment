0db008bf1d703e024e678b6ab4b2ca7bbe6e6e4a
Trevor <wtrevor242@gmail.com>
2021-07-23 18:29:55 -0600

    Submission

--- js/app.js
+++ js/app.js
@@ -1,29 +1,47 @@
-function getApi() {
-    axios.request({
+axios.interceptors.response.use(function (response) {
+    alert("Grabbing list of people for you.");
+    return response;
+  }, function (error) {
+
+    return Promise.reject(error);
+  });
+
+async function getApi() {
+    await axios.request({
         method: "GET",
         url: "https://reqres.in/api/users"
     }).then(useQuote).catch(quoteFailure);
 }
 
 function useQuote(response) {
-    console.log(response);
-    var container = document.createElement('div');
-    container.style.borderBottom = "1px dashed grey";
-    //container.classList.add('myGrid');
-    
-    let fakeAvatar = document.createElement('img');
-    fakeAvatar.setAttribute('src', response.data.data[0].avatar);
-    fakeAvatar.style.width = "200px";
-    container.append(fakeAvatar);
 
-    document.getElementById('pictureUser').append(container);
-    document.getElementById('fName').innerText = response.data.data[0].first_name;
-    document.getElementById('lName').innerText = response.data.data[0].last_name;
-    document.getElementById('email').innerText = response.data.data[0].email;
+    for (let i=0; i < response.data.data.length; i++) {
+        console.log(response);
+        var container = document.createElement('div');
+        container.style.borderBottom = "1px dashed blue";
+        
+        let fakeAvatar = document.createElement('img');
+        fakeAvatar.setAttribute('src', response.data.data[i].avatar);
+        fakeAvatar.style.width = "200px";
+        container.append(fakeAvatar);
+        document.getElementById('pictureUser').append(container);
+
+        let fakeFName = document.createElement('p');
+        fakeFName.innerText = response.data.data[i].first_name;
+        container.append(fakeFName);
+        
+        let fakeLName = document.createElement('p');
+        fakeLName.innerText = response.data.data[i].last_name;
+        container.append(fakeLName);
+
+        let fakeEmail = document.createElement('p');
+        fakeEmail.innerText = response.data.data[i].email;
+        container.append(fakeEmail);
+    }
 }
 
 function quoteFailure(error) {
-    console.error("We failed to get API");
+    console.error("We failed to connect to a list of people");
     console.error(error);
 }
 
